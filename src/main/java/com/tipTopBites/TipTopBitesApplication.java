14cd697c43d2d44c765205bd2a969896ef5ecca3
vaidaabelkyte <vaidilutia@gmail.com>
2017-10-10 22:43:43 +0100

    project set up and index page made using bootstrap

---
+++ src/main/java/com/tipTopBites/TipTopBitesApplication.java
@@ -0,0 +1,12 @@
+package com.tipTopBites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipTopBitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipTopBitesApplication.class, args);
+	}
+}
---
+++ src/main/java/com/tipTopBites/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.tipTopBites.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	@RequestMapping("/myAccount")
+	public String myAccount() {
+		return "myAccount";
+	}
+	
+}
+
+
