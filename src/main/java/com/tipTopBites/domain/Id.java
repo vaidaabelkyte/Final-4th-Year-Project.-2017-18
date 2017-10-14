a61a30feb80ca0906cf93241a11f8ebd4b848237
vaidaabelkyte <vaidilutia@gmail.com>
2017-10-14 13:04:42 +0100

    security added (works with name 'user' and default password generated by Spring)

--- src/main/java/com/tipTopBites/domain/Id.java
+++
@@ -1,5 +0,0 @@
-package com.tipTopBites.domain;
-
-public @interface Id {
-
-}
--- src/main/java/com/tipTopBites/domain/User.java
+++ src/main/java/com/tipTopBites/domain/User.java
@@ -1,13 +1,28 @@
 package com.tipTopBites.domain;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.tipTopBites.domain.security.Authority;
+import com.tipTopBites.domain.security.UserRole;
 
 @Entity
-public class User {
+public class User implements UserDetails {
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="id", nullable=false, updatable=false)
@@ -21,6 +36,11 @@ public class User {
 	private String email;
 	private String phone;
 	private boolean enabled=true;
+	
+	@OneToMany(mappedBy="role",  cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	@JsonIgnore
+	private Set<UserRole> userRoles =new HashSet<>();
+	
 	public Long getId() {
 		return id;
 	}
@@ -63,15 +83,45 @@ public class User {
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	public boolean isEnabled() {
-		return enabled;
-	}
+	
+	
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Set<GrantedAuthority> authorities=new HashSet<>();
+		userRoles.forEach(ur -> authorities.add (new Authority(ur.getRole().getName())) );
+		return authorities;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 	
 	
 	
+	@Override
+	public boolean isEnabled() {
+		return enabled;
+	}
 	
 
 }
